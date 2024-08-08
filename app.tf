1fa6173d210768bcb4f6d8cb733b69493d62906b
Robert Hagen <rdh@stealthllama.org>
2024-08-08 13:00:12 -0400

    Errors oy!

--- app.tf
+++ app.tf
@@ -1,17 +1,17 @@
 # Locals
 
 locals {
-    # wordpress_config = <<EOF
-    #     <?php
-    #     # Created by /usr/share/doc/wordpress/examples/setup-mysql 
-    #     define('DB_NAME', 'Demo');
-    #     define('DB_USER', 'demouser');
-    #     define('DB_PASSWORD', 'paloalto@123');
-    #     define('DB_HOST', '10.5.3.100');
-    #     define('SECRET_KEY', 'UtqouIbh65q92QYevFJzth5Kuya3GKozJzmOq4Mv0mevSmgtlW');
-    #     define('WP_CONTENT_DIR', '/var/lib/wordpress/wp-content');
-    #     ?>
-    #     EOF
+    wordpress_config = <<EOF
+        <?php
+        # Created by /usr/share/doc/wordpress/examples/setup-mysql 
+        define('DB_NAME', 'Demo');
+        define('DB_USER', 'demouser');
+        define('DB_PASSWORD', 'paloalto@123');
+        define('DB_HOST', '10.5.3.100');
+        define('SECRET_KEY', 'UtqouIbh65q92QYevFJzth5Kuya3GKozJzmOq4Mv0mevSmgtlW');
+        define('WP_CONTENT_DIR', '/var/lib/wordpress/wp-content');
+        ?>
+        EOF
     app_script = <<EOF
         #!/bin/bash
         host=updates.paloaltonetworks.com
