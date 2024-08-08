b8e5f37eae3940a8570c9c4db49b2eb1e8ef7d45
Robert Hagen <rdh@stealthllama.org>
2024-08-08 10:55:02 -0400

    Updated outputs

--- outputs.tf
+++ outputs.tf
@@ -1,16 +1,11 @@
 # Outputs
 
-output "firewall-instance" {
-  value = "${google_compute_instance.firewall_instance.self_link}"
-  description = "Firewall link"
-}
-
-output "firewall-public-ip" {
+output "firewall-mgmt-ip" {
   value = "${google_compute_instance.firewall_instance.network_interface.0.access_config.0.nat_ip}"
   description = "The public IP address of the firewall management interface"
 }
 
-output "web-public-ip" {
+output "firewall-untrust-ip" {
   value = "${google_compute_instance.firewall_instance.network_interface.1.access_config.0.nat_ip}"
   description = "The public IP address of the firewall untrust interface"
 }
