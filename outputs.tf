70496fd7745c5b81f2b9788e57a1ea19b8b49d7b
Robert Hagen <rdh@stealthllama.org>
2024-08-08 11:12:49 -0400

    New output

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,11 @@
 # Outputs
 
+output "firewall-link" {
+  value = "${google_compute_instance.firewall_instance.self_link}"
+  description = "The GCP resource link for the firewall instance (for troubleshooting)"
+}
+
+
 output "firewall-mgmt-ip" {
   value = "${google_compute_instance.firewall_instance.network_interface.0.access_config.0.nat_ip}"
   description = "The public IP address of the firewall management interface"
